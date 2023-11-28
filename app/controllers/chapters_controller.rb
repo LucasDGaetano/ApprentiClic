941f0b73052e128e17f7401c02ea39424f01e218
Lucas De Gaetano <lucasdgpro@gmail.com>
2023-11-28 14:33:33 +0100

    progress incremente bien quand on valide un chapitre

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,9 +1,12 @@
 class ChaptersController < ApplicationController
   def update
     @chapter = Chapter.find(params[:id])
-    uc = @chapter.course.users_courses.where(user_id: current_user)
-    uc.progress += (100 / course.chapters.where(done: true).count).round
+    uc = @chapter.course.users_courses.where(user_id: current_user).first
     @chapter.update_attribute(:done, true)
+    diviseur = uc.course.chapters.count
+    uc.progress += (100 / diviseur).round
+    uc.save!
+    # uc.course.chapters.where(done: true).count je garde dans un coin a reutiliser dans le dashboard
     redirect_to course_path(@chapter.course)
   end
 end
