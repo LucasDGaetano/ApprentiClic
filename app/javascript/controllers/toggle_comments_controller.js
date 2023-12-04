5b52e0bd63d08a67c901156e72678e56fdd4450b
Lucas De Gaetano <lucasdgpro@gmail.com>
2023-12-04 16:19:42 +0100

    added js to mask comments in overview and show

---
+++ app/javascript/controllers/toggle_comments_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="toggle-comments"
+export default class extends Controller {
+  static targets = ["toggable"]
+
+  connect() {
+    console.log("hello from toggle comments");
+  }
+
+  fire(event) {
+    this.toggableTarget.classList.toggle('d-none');
+    console.log(event.currentTarget)
+    if (this.toggableTarget.classList.contains('d-none')) {
+      event.currentTarget.innerHTML = "Voir les commentaires"
+    }
+    else {
+      event.currentTarget.innerHTML = "Masquer les commentaires"
+    }
+
+  }
+}
